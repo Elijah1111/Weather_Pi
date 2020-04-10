4938633a087e53c5aa92d3f50fb813535f9fbdce
Elijah1111 <elijahmtcastle@gmail.com>
2020-04-09 21:04:25 -0600

    Preliminary upload logic and some tweaks

--- dataCapture.py
+++ dataCapture.py
@@ -2,35 +2,49 @@ import time
 import adcUtil as adc
 from os import path
 #TODO edit anything with a TODO
+
 #~~~~~~~~~~~~~~~~~~~~Assignment~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 location = "GOLDEN" #TODO Change me based on location
-fName = "data_"+location+".csv" #set up the file name 
+fName = location+"_data.csv" #set up the file name 
 
-tChan = (0,0)#Channel 0, chip 0
-iChan = (1,0)
-lChan = (0,1)#Channel 0,  chip 1
+tChan = (0,0)#Channel 0, chip 0 Temperature
+lChan = (1,0)#Channel 1, chip 0 Light
+aChan = (0,1)#Channel 0,  chip 1 Audio
+eChan = (1,1)#Channel 1,  chip 1 Envelope
+
 #~~~~~~~~~~~~~~~~~~~~Functions~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-def readTemp():#TODO
+
+def readTemp():#Read thermometer
     tmp = adc.readADC(tChan[0],tChan[1])
-    return (100*tmp -50)#equation for tempature
+    return (100*tmp -50) #Change Volts to deg. C
 
-def readInfer():#TODO 
-    return adc.readADC(iChan[0],iChan[1])
+def readAud():#Read Audio
+    return (adc.readADC(aChan[0],aChan[1])-1.65) #-1.65 to adjust audio voltage to 0
 
-def readLight():#TODO
+def readEnv():#Read Audio Envelope
+    return adc.readADC(eChan[0],eChan[1])
+
+def readLight():#Read Photoresistor
     return adc.readADC(lChan[0],lChan[1])
+
 #~~~~~~~~~~~~~~~~~~~~Main~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 tmp = ("{:.3f},".format(readTemp())+#Record Temperature
-        "{:.3f},".format(readInfer())+#Record Infer-red
         "{:.3f},".format(readLight())+#Record Light
+        "{:.3f},".format(readAud())+#Record Audio
+        "{:.3f},".format(readEnv())+#Record Audio Envelope
        "{:.0f}".format(time.time()))#Record time in epoch UTC
+
+
 flag=False
 if(not path.exists(fName)):#prints the header if the file does not exist
     flag=True
 
 with open(fName,'a') as f: #open up the file in append mode
-    if(flag):print("Temp,IR,Light,Time",file=f)#print header
+    if(flag):print("Temp,Light,Time",file=f)#print header
 
     print(tmp, file=f)#print this in the file
     f.close()
+
+print("Data Collected")
---
+++ upload.sh
@@ -0,0 +1,18 @@
+#Uploads the data file
+FILE = "*_data.csv"
+SERVER = "USER@SERVER"
+cat $FILE > tmpD #Backup the data in case the transfer fails
+tail -n +2 $FILE > $FILE #Take out the first line of the file
+if [scp ./$FILE $SERVER] then
+	
+	if [ssh $SERVER "cat $FILE >> MASTER$FILE; rm $FILE"]then #append the file to the master list
+		echo "Appending Complete"; rm $FILE #remove the file
+	else
+		echo "$? Error printing Data" > error.log #hopefully this does not happen but generates a very basic error log
+		cat tmpD > $FILE #put the data back into the file 
+	fi	
+else
+	echo "Error in transporting file"
+	cat tmpD > $FILE #put the data back into the file 
+fi
+rm tmpD #remove the backup
