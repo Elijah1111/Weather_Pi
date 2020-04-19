2d0b0dd22fbb7a2d810499e918c99efcbd639791
Elijah1111 <elijahmtcastle@gmail.com>
2020-04-19 10:21:11 -0600

    Added some comments

--- upload.py
+++ upload.py
@@ -7,20 +7,20 @@ print("Uploading Data")
 fName = glob.glob("*.csv")[0]
 print("Got "+fName)
 
-con = ClientSocket()
-hString="0"
-result = hashlib.md5(hString.encode())
-checksum = result.digest()
+con = ClientSocket()#start client socket
+hString="0"#hasstring I just made it 0 cause why not
+result = hashlib.md5(hString.encode())#encode the hash
+checksum = result.digest()#generate the checksum
 
 with open(fName,'r') as f: #open up the file in read mode
-        con.send(checksum)
-        con.send(f.read())
-        f.close()
+        con.send(checksum)#send the checksum
+        con.send(f.read())#send the file
+        f.close()#close the file
 
-if(con.rec()==200):
+if(con.rec()==200):#All clear condition
     print("Data Uploaded and received correctly")
-    os.remove(fName)
+    os.remove(fName)#remove the file its on the server
 else:
-    print("Error Uploading: Keeping Data")
+    print("Error Uploading: Keeping Data")#Serever did not recive the data keeping data
 
 con.close()
